4e665eb3ce32ec5c72607d16d14fd9093b597dfa
Jon Zivku <jonzivku@me.com>
2021-03-25 17:23:20 -0700

    initialized

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,6 @@
+# Rails.application.config.session_store :cookie_store, key: '_project3_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+Rails.application.config.session_store :active_record_store
