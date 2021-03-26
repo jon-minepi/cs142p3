d74b700fbd247c5d6e6d06f1c8d4ba063126d0b0
Jon Zivku <jonzivku@me.com>
2021-03-25 18:53:58 -0700

    states scaffolding

---
+++ app/models/state.rb
@@ -0,0 +1,17 @@
+class State < ApplicationRecord
+  @@states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California",
+              "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+              "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+              "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+              "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+              "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+              "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+              "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+              "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+              "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+
+  def self.filter(substring: substr)
+    return states.select{|string| string.include?(substr)}
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_001000) do
+ActiveRecord::Schema.define(version: 2021_03_26_014938) do
 
   create_table "sessions", force: :cascade do |t|
     t.string "session_id", null: false
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 2021_03_26_001000) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
